06e4373647b5970ce94b818b8b893600a33371c6
Aarti Venkat <aartiv@uchicago.edu>
2024-03-15 13:52:16 -0500

    updated vcf analysis code

--- nextflow_notebooks/containerized_cpu_workflows/chips/utils/analyze_vcf.py
+++ nextflow_notebooks/containerized_cpu_workflows/chips/utils/analyze_vcf.py
@@ -1,30 +1,83 @@
+#!/usr/bin/env python3
+import argparse
 import sys
 import pandas as pd
-import cyvcf2
+from cyvcf2 import VCF
 
 
-vcf_file = sys.argv[1]
-results_csv = f"{vcf_file}_analysis.csv"
+def analyze_vcf(input_vcf, output_csv, chip_truth_variants):
+    # dump truth variants to a pandas df
+    truth_df = pd.read_csv(chip_truth_variants, sep="\t")
+    # dump sample variants to a pandas df
+    chrom_list = []
+    ref_list = []
+    alt_list = []
+    start_list = []
+    end_list = []
+    for variant in VCF(input_vcf):
+        chrom = variant.CHROM
+        ref = variant.REF
+        alt = variant.ALT[0]
+        start = variant.start
+        end = variant.end
+
+        ref_list.append(ref)
+        alt_list.append(alt)
+        start_list.append(start)
+        end_list.append(end)
+
+    # create a pandas df from variant_dict
+    variant_dict = {
+        "chromosome_name": chrom_list,
+        "start": start_list,
+        "stop": end_list,
+        "reference": ref_list,
+        "variant": alt_list,
+    }
+    sample_variant_df = pd.DataFrame.from_dict(variant_dict)
+
+    # inner join
+    chip_variants = sample_variant_df.join(
+        truth_df,
+        on=["chromosome_name", "start", "stop", "reference", "variant"],
+        how="inner",
+    )
+    return chip_variants
 
 
-def main(vcf_file, results_csv):
-    df = pd.DataFrame()
+def setup_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input-vcf", dest="input_vcf", help="path to input vcf file", required=True
+    )
+    parser.add_argument(
+        "--output-csv",
+        dest="output_csv",
+        help="output csv with CHIP variants",
+        required=True,
+    )
+    parser.add_argument(
+        "--chip-truth-variants",
+        dest="chip_truth_variants",
+        help="truth variants for CHIP",
+        required=True,
+    )
+    args = parser.parse_args()
+    return args
 
-    for variant in vcf_file:
-        if variant.QUAL > 100:
-            d = {
-                "Chrom": variant.CHROM,
-                "Position": variant.POS,
-                "Mutation": variant.REF + "|" + variant.ALT[0],
-                "Quality": variant.QUAL,
-            }
-            df = pd.concat([df, pd.DataFrame([d])], ignore_index=True)
 
-    df["Sample"] = vcf_file[:-4]
-    df["Data Commons"] = "Bio Data Catalyst"
-
-    return df.to_csv(results_csv)
+def main(argv=None):
+    args = setup_args()
+    input_vcf = args.input_vcf
+    output_csv = args.output_csv
+    chip_truth_variants = args.chip_truth_variants
+    chip_variants = analyze_vcf(
+        input_vcf=input_vcf,
+        output_csv=output_csv,
+        chip_truth_variants=chip_truth_variants,
+    )
+    chip_variants.to_csv(output_csv)
 
 
 if __name__ == "__main__":
-    main(vcf_file, results_csv)
+    main()
