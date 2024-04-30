366e8592b5d0e8c60a7bda9d75b7b69cfa71d022
J Montgomery Maxwell <jmontgomerymaxwell@Js-MacBook-Pro.local>
2024-04-30 16:01:32 -0500

    fix midrc workflow

--- nextflow_notebooks/containerized_cpu_workflows/midrc_batch_demo/utils/extract_metadata.py
+++ nextflow_notebooks/containerized_cpu_workflows/midrc_batch_demo/utils/extract_metadata.py
@@ -9,7 +9,7 @@ metadata_csv = f"dicom-metadata-{dicom_input}.csv"
 
 
 def main(dicom_input, metadata_csv):
-    metadata_df = join_tree("", verbose=2)
+    metadata_df = join_tree(dicom_input, verbose=2)
     dicom_metadata_df = metadata_df.loc[metadata_df["PixelRepresentation"].notnull()]
     dicom_metadata_df.drop_duplicates(inplace=True)
     return dicom_metadata_df.to_csv(metadata_csv)
